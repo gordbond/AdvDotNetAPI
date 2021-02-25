ab3ced8960faf3f3ffcdd26269008206baec96f4
gord bond <000786196@mohawkcollege.ca>
2021-02-24 21:24:12 -0500

    Add project files.

---
+++ Models/MedicalDataContext.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AdvDotNetAPI.Models;
+
+namespace AdvDotNetAPI.Models
+{
+    public class MedicalDataContext : DbContext
+    {
+
+        public MedicalDataContext(DbContextOptions<MedicalDataContext> options)
+            : base(options)
+        {
+        }
+
+        //Immunization DBset - used to create queries for instances of Immunizations
+        public DbSet<Immunization> Immunizations { get; set; }
+       
+        //MedicalEntity DBset - used to create queries for instances of MedicalEntity
+        public DbSet<MedicalEntity> MedicalEntities { get; set; }
+
+        //Person DBset - used to create queries for instances of Person
+        public DbSet<Person> People { get; set; }
+
+        //Patient DBset - used to create queries for instances of Patient
+        public DbSet<Patient> Patients { get; set; }
+
+        //Patient DBset - used to create queries for instances of Patient
+        public DbSet<Provider> Providers { get; set; }
+
+        //Provider DBset - used to create queries for instances of Provider
+        public DbSet<Organization> Organizations { get; set; }
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,60 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AdvDotNetAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AdvDotNetAPI
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+            services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "AdvDotNetAPI", Version = "v1" });
+            });
+            services.AddDbContext<MedicalDataContext>(opt => opt.UseSqlServer("Data Source=gordbond2017\\SQLEXPRESS;Initial Catalog=MedicalDB;Integrated Security=True"));
+
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "AdvDotNetAPI v1"));
+            }
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}

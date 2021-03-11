090ce0b9652110b980eb9c460a4350997bbef95f
Tiago Teles <tiago.franco-de-goes-teles@mohawkcollege.ca>
2021-03-10 23:05:52 -0500

    Added statement of authorship

--- Program.cs
+++ Program.cs
@@ -1,3 +1,8 @@
+/* We, Gord Bond, student number 000786196, Tiago Franco de Goes Teles, student number 000786450, 
+ * Olaoluwa Anthony-Egorp, student number 000776467, and Mitchell Aninyang, student number 000796709, 
+ * certify that all code submitted is our own work; that we have not copied it from any other source. 
+ * We also certify that we have not allowed our work to be copied by others.
+ */
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Server.Kestrel.Core;
